f6f74b32edf2cfbaa2fe63d512b349e72ddc7f64
Gromsam <gromsam@gmail.com>
2023-06-08 17:03:25 +0300

    Добавление переменных для подключения к базе в файл конфигурации

--- config.php
+++ config.php
@@ -1,10 +1,17 @@
 <?php
+
+//Заполните переменные для подключения к базе
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$database = '';
+
 include "DBMysql.php";
 include "Controller.php";
 include "User.php";
 
 //Подключение к базе
-$db = new DBMysql('localhost','root', '', '');
+$db = new DBMysql($hostname,$username, $password, $database);
 
 //Инициализация экземпляра для использования в общем контроллере
 new \Controller\Controller($db);
